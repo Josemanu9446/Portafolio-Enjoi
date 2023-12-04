44d8d440ba6d1e73cdfe63a299def03885a27265
Jose Labarca <josemanu9446@gmail.com>
2023-12-04 17:07:13 -0400

    Todo listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,30 @@
 const frmEmail = document.getElementById('frm-email')
 frmEmail.addEventListener('submit', sendEmail)
 
-const serviceId = 'service_yiqaxon'
-const templateId = 'template_0kvd74k'
+const serviceId = 'contact_service'
+const templateId = 'contact_form'
 const apikey = 'O8xdb2-EwLUHyskPS'
 
 function sendEmail(event) {
   event.preventDefault()
   emailjs.init(serviceId)
 
-  emailjs.sendForm(serviceId,templateId,frmEmail,apikey)
+  emailjs.sendForm(serviceId, templateId, frmEmail, apikey, this)
   .then ( result => Swal.fire('Su mensaje se ha enviado con éxito.')  )
   .catch( error => Swal.fire({
     icon: "error",
     title: "Oops...",
     text: "Something went wrong!",
-    footer: '<a href="#">Why do I have this issue?</a>'
   }))
 }
+
+// Funcion de desplazar hacia arriba 
+const desplazarArriba = document.querySelector("#ir-hacia-arriba");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
